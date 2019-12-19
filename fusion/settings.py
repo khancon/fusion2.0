551f97999a13ac22716e6fed7c107f1a5258ae2d
Ahnaf Khan <31942422+khancon@users.noreply.github.com>
2019-12-19 18:39:33 -0500

    bootstrap changes to navbar

--- fusion/settings.py
+++ fusion/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
     'fusionapp.apps.FusionappConfig',
+    'phonenumber_field',
     'crispy_forms',
 ]
 
--- fusionapp/models.py
+++ fusionapp/models.py
@@ -1,9 +1,12 @@
 # users/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import RegexValidator
 
 class CustomUser(AbstractUser):
-    pass
+    email = models.EmailField(max_length = 254) 
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phonenumber = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
     # add additional fields in here
 
     def __str__(self):
